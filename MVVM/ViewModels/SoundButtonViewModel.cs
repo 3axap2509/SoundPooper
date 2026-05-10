c6425196626e1a8a025ad9de747787065e16c001
3axap2509 <3axap2509@gmail.com>
2026-05-10 15:09:01 +0300

    added vm's for sound-buttons (inherited from action-buttons)

---
+++ MVVM/ViewModels/SoundButtonViewModel.cs
@@ -0,0 +1,37 @@
+using System.Drawing;
+using SoundPooper.Infrastructure.Enums;
+using SoundPooper.Infrastructure.Services;
+
+namespace SoundPooper.MVVM.ViewModels;
+
+public class SoundButtonViewModel : ActionButtonViewModel
+{
+    protected string Path { get; init; }
+    public Icon? Icon { get; set; }
+    public int? Height { get; init; }
+    public int? Width { get; init; }
+
+    public SoundButtonViewModel()
+    {
+    }
+
+    public SoundButtonViewModel(
+        ISoundService soundService,
+        string title,
+        string path,
+        int height,
+        int width)
+        : base(soundService, ButtonFunctionEnum.PlaySound, title)
+    {
+        Path = path;
+        Height = height;
+        Width = width;
+    }
+
+    protected override void SetActionToExecute(object? e)
+    {
+        if (ButtonFunction != ButtonFunctionEnum.PlaySound)
+            throw new InvalidOperationException();
+        _soundService.SetPlaySoundAction(Path);
+    }
+}
