fcec74e944abd668256f491b9307d6cc0c9be04e
3axap2509 <3axap2509@gmail.com>
2026-05-11 05:52:38 +0300

    added 'LabelBackground' dependency-property for button-elements

--- MVVM/Extensions/ButtonExtensions.cs
+++ MVVM/Extensions/ButtonExtensions.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Media;
 
 namespace SoundPooper.MVVM.Extensions;
 
@@ -11,9 +12,23 @@ public static class ButtonExtensions
             typeof(ButtonExtensions),
             new PropertyMetadata(new CornerRadius(0)));
 
+    public static readonly DependencyProperty LabelBackgroundProperty =
+        DependencyProperty.RegisterAttached(
+            "LabelBackground",
+            typeof(Brush),
+            typeof(ButtonExtensions),
+            new PropertyMetadata(Brushes.Transparent));
+
     public static void SetCornerRadius(UIElement element, CornerRadius value)
         => element.SetValue(CornerRadiusProperty, value);
 
     public static CornerRadius GetCornerRadius(UIElement element)
         => (CornerRadius)element.GetValue(CornerRadiusProperty);
+
+
+    public static void SetLabelBackground(UIElement element, Brush value)
+        => element.SetValue(LabelBackgroundProperty, value);
+
+    public static CornerRadius GetLabelBackground(UIElement element)
+        => (CornerRadius)element.GetValue(LabelBackgroundProperty);
 }
